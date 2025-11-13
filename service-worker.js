96ca17c3854bf60ff9d922087fe7b5b8652d560f
Download <afradiolatina@gmail.com>
2025-11-13 19:43:10 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,46 @@
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open("afradio-cache").then(cache => {
-    return cache.addAll(["./", "./index.html", "./LogoAfradio.jpg"]);
-  }));
+const CACHE_NAME = "afradio-cache-v2";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./LogoAfradio.jpg",
+  "./AFRadio_logo_192.png",
+  "./AFRadio_logo_512.png",
+  "./service-worker.js",
+  "https://afradio.es/listen/afradio/radio.mp3"
+];
+
+// Instalar y guardar en caché los archivos
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Archivos cacheados correctamente");
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+
+// Activar y limpiar cachés antiguos
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) {
+          console.log("Eliminando caché antigua:", key);
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
 });
+
+// Interceptar peticiones y responder desde caché o red
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
+
